a40f1bb046dd94a6f7a9be296e453f8c28d80586
Ojigbo Leo <ojigboleo@gmail.com>
2023-08-19 08:51:25 +0000

    first commit

---
+++ trybe-box/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1 className="text-2xl">TRYBE BOX TV</h1>
+      <h3>Coming Soon</h3>
+    </main>
+  );
+}
