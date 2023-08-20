94406f6ea3788a892cea0de256d9fa5ea205e1a0
Ojigbo Leo <ojigboleo@gmail.com>
2023-08-20 11:19:31 +0000

    amp

--- trybe-box/app/page.js
+++ trybe-box/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
-      <h1 className="text-2xl">TRYBE BOX TV</h1>
+      <h1 className="text-2xl">Tribebox TV</h1>
       <h3>Coming Soon</h3>
     </main>
   );
